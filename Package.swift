b2edf0c695aa8d10c320669b6df0a616f5508a5c
Alexandre Cesar <acmorcego@gmail.com>
2018-08-29 18:18:22 -0300

    init swift package

---
+++ Package.swift
@@ -0,0 +1,14 @@
+// swift-tools-version:4.0
+
+import PackageDescription
+
+let package = Package(
+    name: "tsp-heuristics",
+    dependencies: [
+    ],
+    targets: [
+        .target(
+            name: "tsp-heuristics",
+            dependencies: []),
+    ]
+)
