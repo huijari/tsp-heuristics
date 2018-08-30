a4c5fff0395fa82f5854d13d8024dd7da2d3d17c
Alexandre Cesar <acmorcego@gmail.com>
2018-08-29 21:52:42 -0300

    read instance from console

--- Sources/tsp-heuristics/instance.swift
+++ Sources/tsp-heuristics/instance.swift
@@ -32,4 +32,39 @@ struct Instance {
 	var dimension = 0
 	var edgeWeightType = WeightType.euclidean
 	var nodes: [Node] = []
+
+	static func readFromConsole() -> Instance {
+		func value(from: String) -> String {
+			return from.split(separator: ":")[1].trimmingCharacters(in: .whitespaces)
+		}
+
+		var meta = true
+		var instance = Instance()
+		var line = readLine() ?? "EOF"
+
+		while line != "EOF" {
+			if meta {
+					if line.starts(with: "NODE_COORD_SECTION") { meta = false }
+					if line.starts(with: "NAME") {
+						instance.name = value(from: line)
+					}
+					if line.starts(with: "DIMENSION") {
+						instance.dimension = Int(value(from: line))!
+					}
+					if line.starts(with: "EDGE_WEIGHT_TYPE") {
+						let type = value(from: line)
+						instance.edgeWeightType = WeightType(rawValue: type)!
+					}
+			} else {
+				let components = line.split(separator: " ")
+				let x = Int(components[1])!
+				let y = Int(components[2])!
+				instance.nodes.append(Node(x: x, y: y))
+			}
+
+			line = readLine() ?? "EOF"
+		}
+
+		return instance
+	}
 }
